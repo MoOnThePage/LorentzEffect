d1795af34b2b143eac5b470094f3d1054e867b61
MAAL <moonthepage@gmail.com>
2025-11-29 18:36:20 +0200

    getting started

--- basics/main.py
+++ basics/main.py
@@ -1,13 +1,23 @@
 from manim import *
-class DefaultTemplate(Scene):
+
+# Animate a circle
+class CreateCircle(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+        circle = Circle() # Create a circle
+        circle.set_fill(PINK, opacity=0.5) # set the color and transparency
+        self.play(Create(circle)) # show the circle on the screen
 
-        square = Square()  # create a square
-        square.flip(RIGHT)  # flip horizontally
-        square.rotate(-3 * TAU / 8)  # rotate a certain amount
+# Transforming a square into a circle
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
 
-        self.play(Create(square))  # animate the creation of the square
-        self.play(Transform(square, circle))  # interpolate the square into the circle
-        self.play(FadeOut(square))  # fade out animation
+        square = Square()
+        square.rotate(PI/4)
+
+        self.play(Create(square))
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
+
+# TODO: Next Positioning Mobject S
